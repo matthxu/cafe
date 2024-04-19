ea06099b382f9d77412a5edaf6b465e7cd93eb6b
Matthew <128595283+matthxu@users.noreply.github.com>
2024-04-19 15:09:47 +1000

    Added script to dynamically adjust height of animation block

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+const animationHeight = document.querySelector(".graphic-banner");
 const navigationHeight = document.querySelector("#navigation-bar").offsetHeight;
+const deliveryPageHeight =
+    document.querySelector("#delivery-page").offsetHeight;
+const locationPageHeight =
+    document.querySelector("#location-page").offsetHeight;
 
+// Offsets scroll in relation to navbar
 document.documentElement.style.setProperty(
     "--scroll-padding",
     navigationHeight - 0.9 + "px"
 );
+
+// Dynamically changes element size to fit within screen
+let totalHeight =
+    1080 - deliveryPageHeight - locationPageHeight - navigationHeight - 120;
+// 100px roughly accounting for user taskbar + browser head
+animationHeight.style.height = totalHeight + "px";
